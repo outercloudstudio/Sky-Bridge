6403cc6cfa085607907ffd401ccdd988b264a603
Outer Cloud Studio <outercloudstudio@gmail.com>
2022-06-12 20:12:19 -0500

    Handle Remote Functions

--- Assets/SkyBridge.cs
+++ Assets/SkyBridge.cs
@@ -19,6 +19,14 @@ namespace SkyBridge {
             }
         }
 
+        public class RemoteFunction
+        {
+            public string ID;
+
+            public delegate void Handler(Connection connection, string source, Packet packet);
+            public Handler onHandler;
+        }
+
         public class Room
         {
             public string ID;
@@ -36,14 +44,31 @@ namespace SkyBridge {
         public static Client client;
         public static Connection connection;
 
+        public static List<RemoteFunction> remoteFunctions = new List<RemoteFunction>();
+
         private void Start()
         {
             connection.onPacketRecieved = HandlePacket;
+
+            AddRemoteFunction("DEBUG", onDebug);
+        }
+
+        public void onDebug(Connection connection, string source, Packet packet)
+        {
+            Debug.Log(packet.GetString(0));
         }
 
         private void Update()
         {
             connection.Update(Time.deltaTime);
+        }
+
+        public static void AddRemoteFunction(string ID, RemoteFunction.Handler handler)
+        {
+            remoteFunctions.Add(new RemoteFunction() { 
+                ID = ID,
+                onHandler = handler
+            });
         }
 
         public static void SendSmartPacket(Packet packet, string target)
@@ -57,9 +82,21 @@ namespace SkyBridge {
 
         public void HandlePacket(Connection connection, Packet packet)
         {
-            if(packet.packetType == "DEBUG")
+            if (packet.packetType == "DEBUG")
             {
                 Debug.Log(packet.GetString(0));
+            }else if (packet.packetType == "RELAY")
+            {
+                string ID = packet.GetString(0);
+                string source = packet.GetString(1);
+
+                Packet newPacket = new Packet(packet.packetType);
+                newPacket.values = packet.values.GetRange(2, packet.values.Count - 2);
+
+                foreach (RemoteFunction remoteFunction in remoteFunctions.FindAll(remoteFunction => remoteFunction.ID == ID))
+                {
+                    remoteFunction.onHandler(connection, source, newPacket);
+                }
             }
         }
     }
